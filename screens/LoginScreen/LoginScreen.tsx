ddf343b0692188eaf0248ea9d9d38749f120fd92
Aboubacar Diall <a.diallo3012@gmail.com>
2022-11-16 11:15:12 +0100

    Refonte du frontend signin et signup

--- screens/LoginScreen/LoginScreen.tsx
+++ screens/LoginScreen/LoginScreen.tsx
@@ -1,14 +1,39 @@
 import { useState } from 'react'
-import { View, TextInput } from 'react-native'
+import { View, TextInput, StyleSheet, Dimensions, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import CustomButton from '@components/CustomButton/CustomButton'
 import { useRegisterMutation } from '../../graphql/graphql'
+
+//Icons
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+
+// Native Base
+import {
+  NativeBaseProvider,
+  Input,
+  Stack,
+  Icon,
+  Center,
+  Box,
+  Heading,
+  VStack,
+  FormControl,
+  Link,
+  HStack,
+  Text,
+  ScrollView,
+  Badge,
+  Button,
+} from 'native-base'
 
 interface LoginScreenProps {}
 
 const LoginScreen: React.FunctionComponent<LoginScreenProps> = ({}) => {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [show, setShow] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>('')
+
   const [verifiedPassword, setVerifiedPassword] = useState<string>('')
   const [firstName, setFirstName] = useState<string>('')
   const [lastName, setLastName] = useState<string>('')
@@ -30,49 +55,180 @@ const LoginScreen: React.FunctionComponent<LoginScreenProps> = ({}) => {
     })
     console.log('response', response)
   }
+  const handleSubmitForm = () => {
+    console.log(email, password)
+  }
   return (
-    <View className='flex-1 items-center justify-center bg-blue-100'>
-      <CustomButton buttonTitle='Go Home' onPress={goHome} />
-      <CustomButton buttonTitle='Dev' onPress={goDev} />
-      <View className='mt-10 '>
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setEmail(val)}
-          value={email}
-          placeholder='Adresse Email'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setPassword(val)}
-          value={password}
-          secureTextEntry={true}
-          placeholder='Mot de passe'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setVerifiedPassword(val)}
-          value={verifiedPassword}
-          secureTextEntry={true}
-          placeholder='Mot de passe'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setFirstName(val)}
-          value={firstName}
-          placeholder='First Name'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setLastName(val)}
-          value={lastName}
-          placeholder='Last Name'
-        />
-        <CustomButton buttonTitle='Sign-up' onPress={signUp} />
+    // <View className='flex-1 items-center justify-center bg-blue-100'>
+    //   <CustomButton buttonTitle='Go Home' onPress={goHome} />
+    //   <CustomButton buttonTitle='Dev' onPress={goDev} />
+    //   <View className='mt-10 '>
+    //     <TextInput
+    //       className='bg-red-300'
+    //       onChangeText={(val) => setEmail(val)}
+    //       value={email}
+    //       placeholder='Adresse Email'
+    //     />
+    //     <TextInput
+    //       className='bg-red-300'
+    //       onChangeText={(val) => setPassword(val)}
+    //       value={password}
+    //       secureTextEntry={true}
+    //       placeholder='Mot de passe'
+    //     />
+    //     <TextInput
+    //       className='bg-red-300'
+    //       onChangeText={(val) => setVerifiedPassword(val)}
+    //       value={verifiedPassword}
+    //       secureTextEntry={true}
+    //       placeholder='Mot de passe'
+    //     />
+    //     <TextInput
+    //       className='bg-red-300'
+    //       onChangeText={(val) => setFirstName(val)}
+    //       value={firstName}
+    //       placeholder='First Name'
+    //     />
+    //     <TextInput
+    //       className='bg-red-300'
+    //       onChangeText={(val) => setLastName(val)}
+    //       value={lastName}
+    //       placeholder='Last Name'
+    //     />
+    //     <CustomButton buttonTitle='Sign-up' onPress={signUp} />
 
-        <CustomButton buttonTitle='Sign-in' onPress={() => navigation.navigate('SignIn')} />
+    //     <CustomButton buttonTitle='Sign-in' onPress={() => navigation.navigate('SignIn')} />
+    //   </View>
+    // </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image source={require('../../assets/logoTBP.png')} style={styles.logo} />
+        <Center w='100%'>
+          <Box safeArea w='90%' maxW='290'>
+            <Center>
+              <Heading
+                size='lg'
+                fontWeight='600'
+                color='coolGray.800'
+                _dark={{
+                  color: 'warmGray.50',
+                }}
+                textAlign='center'
+              >
+                Bienvenue dans chez boatProject
+              </Heading>
+
+              <Heading
+                mt='1'
+                _dark={{
+                  color: 'warmGray.200',
+                }}
+                color='coolGray.600'
+                fontWeight='medium'
+                size='xs'
+              >
+                Connectez-vous pour continuer
+              </Heading>
+            </Center>
+
+            <VStack space={3} mt='5'>
+              <FormControl>
+                <FormControl.Label>Email</FormControl.Label>
+                <Input
+                  value={email}
+                  onChangeText={(text) => setEmail(text)}
+                  size='2xl'
+                  InputLeftElement={
+                    <Icon as={<Ionicons name='mail' />} size={5} ml='2' color='muted.400' />
+                  }
+                />
+              </FormControl>
+              <FormControl>
+                <FormControl.Label>Mot de passe</FormControl.Label>
+                <Input
+                  value={password}
+                  onChangeText={(password) => setPassword(password)}
+                  size='2xl'
+                  type={show ? 'text' : 'password'}
+                  InputRightElement={
+                    <Icon
+                      as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
+                      size={5}
+                      mr='2'
+                      color='muted.400'
+                      onPress={() => setShow(!show)}
+                    />
+                  }
+                />
+                <Link
+                  _text={{
+                    fontSize: 'xs',
+                    fontWeight: '500',
+                    color: 'orange.500',
+                  }}
+                  alignSelf='flex-end'
+                  mt='1'
+                >
+                  Mot de passe oublié?
+                </Link>
+              </FormControl>
+              <Button
+                bg='blue.500'
+                _text={{
+                  fontSize: 'lg',
+                  fontWeight: '500',
+                }}
+                onPressIn={() => handleSubmitForm()}
+                mt='2'
+              >
+                Se connecter
+              </Button>
+              {errorMessage && <Text color='red.900'>{errorMessage}</Text>}
+              <HStack mt='1' justifyContent='center'>
+                <Text
+                  fontSize='sm'
+                  color='coolGray.600'
+                  _dark={{
+                    color: 'warmGray.200',
+                  }}
+                >
+                  Pas encore de compte?{' '}
+                </Text>
+                <Link
+                  onPress={() => navigation.navigate('SignUp')}
+                  _text={{
+                    color: 'orange.500',
+                    fontWeight: 'medium',
+                    fontSize: 'sm',
+                  }}
+                  href='#'
+                >
+                  Créer un compte
+                </Link>
+              </HStack>
+            </VStack>
+          </Box>
+        </Center>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
 export default LoginScreen
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Dimensions.get('window').width * 0.1,
+    paddingBottom: Dimensions.get('window').width * 0.1,
+    flex: 1,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: "center",
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    objectFit: 'cover',
+    borderRadius: 100,
+  },
+})
--- screens/SignUpScreen/SignUpScreen.tsx
+++ screens/SignUpScreen/SignUpScreen.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { View, TextInput } from 'react-native'
+import { View, TextInput, StyleSheet, Image } from 'react-native'
 import { useReactiveVar } from '@apollo/client'
 
 import { useNavigation } from '@react-navigation/native'
@@ -9,15 +9,37 @@ import { setAccessToken } from '../../accessToken'
 import { accessTokenVar } from '../../variables/accessToken'
 import { userDataVar } from '../../variables/userData'
 
+// Native Base
+import {
+  NativeBaseProvider,
+  Input,
+  Stack,
+  Icon,
+  Center,
+  Box,
+  Heading,
+  VStack,
+  FormControl,
+  Link,
+  HStack,
+  Text,
+  ScrollView,
+  Modal,
+  Button,
+} from 'native-base'
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+
 interface SignUpScreenProps {}
 
 const SignUpScreen: React.FunctionComponent<SignUpScreenProps> = ({}) => {
   const [email, setEmail] = useState<string>('')
-
   const [password, setPassword] = useState<string>('')
   const [verifiedPassword, setVerifiedPassword] = useState<string>('')
   const [firstName, setFirstName] = useState<string>('')
   const [lastName, setLastName] = useState<string>('')
+  const [show, setShow] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>()
+
   const [register] = useRegisterMutation()
 
   const navigation = useNavigation()
@@ -25,74 +47,217 @@ const SignUpScreen: React.FunctionComponent<SignUpScreenProps> = ({}) => {
   const goDev = () => navigation.navigate('Dev')
 
   const signUp = async () => {
-    const response = await register({
-      variables: {
-        newUsersInput: {
-          email,
-          password,
-          firstName,
-          lastName,
+    if (validateEmail(email) && validatePassword(password)) {
+      const response = await register({
+        variables: {
+          newUsersInput: {
+            email,
+            password,
+            firstName: firstName.trim(),
+            lastName: lastName.trim(),
+          },
         },
-      },
-    })
-    if (response && response.data) {
-      console.log('REPONSE', response)
-      setAccessToken(response.data.createUsers.accessToken, response.data.createUsers.refreshToken)
-      accessTokenVar(response.data.createUsers.accessToken)
-      userDataVar({
-        firstName: response.data.createUsers.firstName,
-        lastName: response.data.createUsers.lastName,
-        avatar: response.data.createUsers.avatar,
-        status: response.data.createUsers.status,
-        email: response.data.createUsers.email,
       })
 
-      navigation.navigate('BottomTabs')
+      if (response && response.data) {
+        console.log('REPONSE', response)
+        setAccessToken(
+          response.data.createUsers.accessToken,
+          response.data.createUsers.refreshToken,
+        )
+        accessTokenVar(response.data.createUsers.accessToken)
+        userDataVar({
+          firstName: response.data.createUsers.firstName,
+          lastName: response.data.createUsers.lastName,
+          avatar: response.data.createUsers.avatar,
+          status: response.data.createUsers.status,
+          email: response.data.createUsers.email,
+        })
+
+        navigation.navigate('BottomTabs')
+      }
     }
   }
-  return (
-    <View className='flex-1 items-center justify-center bg-blue-100'>
-      <CustomButton buttonTitle='Go Home' onPress={goHome} />
-      <CustomButton buttonTitle='Dev' onPress={goDev} />
-      <View className='mt-10 '>
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setEmail(val)}
-          value={email}
-          placeholder='Adresse Email'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setPassword(val)}
-          value={password}
-          secureTextEntry={true}
-          placeholder='Mot de passe'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setVerifiedPassword(val)}
-          value={verifiedPassword}
-          secureTextEntry={true}
-          placeholder='Mot de passe'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setFirstName(val)}
-          value={firstName}
-          placeholder='First Name'
-        />
-        <TextInput
-          className='bg-red-300'
-          onChangeText={(val) => setLastName(val)}
-          value={lastName}
-          placeholder='Last Name'
-        />
-        <CustomButton buttonTitle='Sign-up' onPress={signUp} />
+  // Variables
+  const email_pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/gi
+  const password_pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/gi
 
-        <CustomButton buttonTitle='Sign-in' onPress={() => navigation.navigate('SignIn')} />
-      </View>
+  // functions
+
+  const validateEmail = (email: string) => {
+    if (email_pattern.test(email.trim().toLowerCase())) return true
+    setErrorMessage('Email incorrect, verifiez bien votre email et ressayer ')
+    return false
+  }
+  const validatePassword = (password: string) => {
+    if (password_pattern.test(password)) return true
+    setErrorMessage(
+      'Mot de passe incorrect, Votre mot de passe doit contenir au moins [8 caracteres avec au moins 1 caractere special]',
+    )
+    return false
+  }
+  return (
+    <View
+      className='text-xl  color-deepBlue font-ralewayBold
+      '
+      style={{ backgroundColor: '#fff', height: '100%' }}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <Image source={require('../../assets/logoTBP.png')} style={styles.logo} />
+          <Center w='100%'>
+            <Box safeArea w='90%' maxWidth='290'>
+              <Center>
+                <Heading
+                  size='lg'
+                  fontWeight='600'
+                  color='coolGray.800'
+                  _dark={{
+                    color: 'warmGray.50',
+                  }}
+                  textAlign='center'
+                >
+                  Bienvenue Boat Project
+                </Heading>
+                <Heading
+                  mt='1'
+                  _dark={{
+                    color: 'warmGray.200',
+                  }}
+                  color='coolGray.600'
+                  fontWeight='medium'
+                  size='xs'
+                >
+                  Inscrivez-vous pour continuer
+                </Heading>
+              </Center>
+              <VStack space={3} mt='5'>
+                <FormControl>
+                  <FormControl.Label>Prenom</FormControl.Label>
+                  <Input
+                    value={firstName}
+                    onChangeText={(text) => setFirstName(text)}
+                    size='2xl'
+                    InputLeftElement={
+                      <Icon as={<Ionicons name='person' />} size={5} ml='2' color='muted.400' />
+                    }
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label>Nom</FormControl.Label>
+                  <Input
+                    value={lastName}
+                    onChangeText={(text) => setLastName(text)}
+                    size='2xl'
+                    InputLeftElement={
+                      <Icon as={<Ionicons name='person' />} size={5} ml='2' color='muted.400' />
+                    }
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label>Email</FormControl.Label>
+                  <Input
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    size='2xl'
+                    InputLeftElement={
+                      <Icon as={<Ionicons name='mail' />} size={5} ml='2' color='muted.400' />
+                    }
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label>Mot de passe</FormControl.Label>
+                  <Input
+                    value={password}
+                    onChangeText={(password) => setPassword(password)}
+                    size='2xl'
+                    type={show ? 'text' : 'password'}
+                    InputRightElement={
+                      <Icon
+                        as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
+                        size={5}
+                        mr='2'
+                        color='muted.400'
+                        onPress={() => setShow(!show)}
+                      />
+                    }
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormControl.Label>Confirmer le mot de passe</FormControl.Label>
+                  <Input
+                    value={verifiedPassword}
+                    onChangeText={(password) => setVerifiedPassword(password)}
+                    size='2xl'
+                    type={show ? 'text' : 'password'}
+                    InputRightElement={
+                      <Icon
+                        as={<MaterialIcons name={show ? 'visibility' : 'visibility-off'} />}
+                        size={5}
+                        mr='2'
+                        color='muted.400'
+                        onPress={() => setShow(!show)}
+                      />
+                    }
+                  />
+                </FormControl>
+                <Button
+                  onPressIn={() => signUp()}
+                  mt='2'
+                  bg='blue.500'
+                  _text={{
+                    fontSize: 'lg',
+                    fontWeight: '500',
+                  }}
+                >
+                  Creer mon compte
+                </Button>
+                {errorMessage && <Text color='red.900'>{errorMessage}</Text>}
+                <HStack mt='6' mb='6' justifyContent='center'>
+                  <Text
+                    fontSize='sm'
+                    color='coolGray.600'
+                    _dark={{
+                      color: 'warmGray.200',
+                    }}
+                  >
+                    J'ai dejà un compte?{' '}
+                  </Text>
+                  <Link
+                    onPress={() => navigation.navigate('Profile')}
+                    _text={{
+                      color: 'blue.800',
+                      fontWeight: 'medium',
+                      fontSize: 'sm',
+                    }}
+                    href='#'
+                  >
+                    Se connecter
+                  </Link>
+                </HStack>
+              </VStack>
+            </Box>
+          </Center>
+        </View>
+      </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    flex: 1,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: "center",
+  },
+  logo: {
+    marginTop: 10,
+    width: '95%',
+    height: 80,
+    backgroundSize: 'cover',
+  },
+})
 
 export default SignUpScreen
